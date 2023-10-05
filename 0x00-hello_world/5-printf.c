a4a022e96edf75d591a816c8da8702959a989951
Adam Ouhliss <adamouhliss@gmail.com>
2023-10-05 14:38:48 -0700

    scripts added

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* Description: This is a C script that uses prints function
+* to print a message to the standard output.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
