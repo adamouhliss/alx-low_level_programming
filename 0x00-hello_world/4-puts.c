dd54ce53298dd880171f8326b5680fd155d773cc
Adam Ouhliss <adamouhliss@gmail.com>
2023-10-05 14:32:28 -0700

    script added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 /**
- * * main - Entry point
- * *
- * * Description: This is a C script that uses puts function
- * * to print a message to the standard output.
- * *
- * * Return: Always 0 (Success)
- * */
-int main(void){
+* main - Entry point
+*
+* Description: This is a C script that uses puts function
+* to print a message to the standard output.
+* 
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
