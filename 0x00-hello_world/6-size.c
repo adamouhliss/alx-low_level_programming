838c8621769c4123575035a0537f432c8ccb5907
Adam Ouhliss <adamouhliss@gmail.com>
2023-10-05 17:17:00 -0700

    script 6 added

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,11 +9,10 @@
 */
 int main(void)
 {
-	printf("size of a char: %d bytes\n", sizeof(char));
-	printf("size of a double: %d bytes\n", sizeof(double));
-	printf("size of int: %d bytes\n", sizeof(int));
-	printf("size of float: %d bytes\n", sizeof(float));
-	printf("size of long: %d bytes\n", sizeof(long int));
-	printf("size of long long: %d bytes\n", sizeof(long long int));
-	return (0);
+		printf("Size of a char: %lu byte(s)\n", sizeof(char));
+		printf("Size of an int: %lu byte(s)\n", sizeof(int));
+		printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+		printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+		printf("Size of a float: %lu byte(s)\n", sizeof(float));
+		return (0);
 }
